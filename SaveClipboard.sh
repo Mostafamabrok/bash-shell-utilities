0df0559fea3a111ba4891f9489e78aabaed2d503
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2023-09-07 17:51:02 +0300

    Added a comment on line 4

--- SaveClipboard.sh
+++ SaveClipboard.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Paste the content of the clipboard into the terminal and then press Ctrl+D."
-cat > SavedContent.txt
+cat > SavedContent.txt #asks the user for input and then outputs that into SavedContent.txt
 echo "Clipboard has been saved"
