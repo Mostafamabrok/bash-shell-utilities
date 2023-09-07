7a8ca1adf0baca07444fe695043232412ed1f970
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2023-09-07 17:52:39 +0300

    Fixed punctuation error in string

--- SaveClipboard.sh
+++ SaveClipboard.sh
@@ -2,4 +2,4 @@
 
 echo "Paste the content of the clipboard into the terminal and then press Ctrl+D."
 cat > SavedContent.txt #asks the user for input and then outputs that into SavedContent.txt
-echo "Clipboard has been saved"
+echo "Clipboard has been saved."
