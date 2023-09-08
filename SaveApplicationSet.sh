1324d033302ae01e8a364f6956ee99c36bbe1af0
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2023-09-08 17:24:29 +0300

    Added some echo statements for ease of use.

--- SaveApplicationSet.sh
+++ SaveApplicationSet.sh
@@ -4,21 +4,24 @@ echo "Would you like to add to, view, delete, or exeucte your set  of saved appl
 read Chosen_action
 
 if [[ "$Chosen_action" == "a" ]]; then
-    read Chosen_add
+    read -p "Write the name of the application you want to add, then press enter: " Chosen_add
     echo -e "\n$Chosen_add" >> SavedApps.txt
 fi
 
 if [[ "$Chosen_action" == "v" ]]; then
+    echo "Displaying Saved Apps..."
     cat SavedApps.txt
 fi
 
 if [[ "$Chosen_action" == "d" ]]; then
     rm SavedApps.txt
+    echo "Saved application list deleted."
 fi
 
 if [[ "$Chosen_action" == "e" ]]; then
     for line in $(cat SavedApps.txt)
     do
-        eval "$line"   #printing the line; perform any other operation on line variable
+        eval "$line"
     done
+    echo "Applications executed."
 fi
